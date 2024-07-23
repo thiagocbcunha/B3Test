28cde07a904b3898bbf17affe14369a18178c05b
Thiago Cardoso Barbosa <thiagocbcunha@gmail.com>
2024-07-23 17:44:21 -0300

    Acrescimo coluna

--- src/B3.Test-FrontrEnd/src/app/app.module.ts
+++ src/B3.Test-FrontrEnd/src/app/app.module.ts
@@ -1,11 +1,15 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
+import { CommonModule, registerLocaleData, DecimalPipe } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CDBInvSimulationFormComponent } from './cdb-investment-simulation/cdb-inv-simulation-form.component';
+
+import localePt from '@angular/common/locales/pt';
+
+registerLocaleData(localePt);
 
 @NgModule({
   imports: [
@@ -18,7 +22,7 @@ import { CDBInvSimulationFormComponent } from './cdb-investment-simulation/cdb-i
     AppComponent,
     CDBInvSimulationFormComponent
   ],
-  providers: [],
+  providers: [{ provide: LOCALE_ID, useValue: 'pt' }],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
